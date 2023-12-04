ccb1a5b46dfd331107ad4f56a7ae1f0304ca59c2
1Bishop <chris@bishop.dev>
2023-12-04 13:21:41 -0500

    Completed Lesson 2

--- blocks_basics.rb
+++ blocks_basics.rb
@@ -1,4 +1,28 @@
 # frozen_string_literal: true
 
-puts "Let's practice everything."
+1.upto(5) do |i|
+  puts "#{i}. situp"
+  puts "#{i}. pushup"
+  puts "#{i}. chinup"
+end
 
+class Flyer
+  attr_reader :name, :email, :miles_flown
+
+  def initialize(name, email, miles_flown)
+    @name = name
+    @email = email
+    @miles_flown = miles_flown
+  end
+
+  def to_s
+    "#{name} (#{email}): #{miles_flown}"
+  end
+end
+
+flyers = []
+1.upto(5) do |i|
+  flyers << Flyer.new("Flyer #{i}", "flyer#{i}@localhost", i * 1000)
+end
+
+puts flyers
